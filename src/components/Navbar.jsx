f034033dba7cfc06ffb4c6797b30f9a4bd2f7e14
Luis Sanchez <luisanchezdh@gmail.com>
2025-12-26 21:56:56 -0500

    added lucide icons to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
+import { LayoutDashboard, CreditCard, PiggyBank, FileUp } from "lucide-react";
 
 const Navbar = () => {
   return (
     <nav className="flex flex-col gap-y-5 m-5">
       <h2 className="">FINANCE FLOW</h2>
-      <Link to="/">Dashboard</Link>
-      <Link to="/transactions">Transactions</Link>
-      <Link to="/budgets">Budgets</Link>
-      <Link to="/import">Import</Link>
+      <div className="flex flex-nowrap gap-2">
+        <LayoutDashboard />
+        <Link to="/">Dashboard</Link>
+      </div>
+      <div className="flex flex-nowrap gap-2">
+        <CreditCard />
+        <Link to="/transactions">Transactions</Link>
+      </div>
+      <div className="flex flex-nowrap gap-2">
+        <PiggyBank />
+        <Link to="/budgets">Budgets</Link>
+      </div>
+      <div className="flex flex-nowrap gap-2">
+        <FileUp />
+        <Link to="/import">Import</Link>
+      </div>
     </nav>
   );
 };
