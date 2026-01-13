87a36650791364b2aef7ee223bd3233726796d62
Luis Sanchez <luisanchezdh@gmail.com>
2026-01-13 12:10:59 -0500

    displaying total balance, expenses, income on cards, cleaned up file with redundant code

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,35 +4,48 @@ import { BanknoteArrowDown } from "lucide-react";
 import { WalletCards } from "lucide-react";
 
 const Dashboard = ({ transactionList }) => {
-  function totalBalance() {
-    let totalAmount = 0;
-    const expense = transactionList.filter((tran) => tran.type === "expense");
-    console.log(expense);
-    const sumOfExpense = expense.reduce(
+  const expense = transactionList.filter((tran) => tran.type === "expense");
+  const income = transactionList.filter((tran) => tran.type === "income");
+
+  function sumOfIncome() {
+    const total = income.reduce(
       (sum, transaction) => sum + parseFloat(transaction.amount),
-      totalAmount
+      0
     );
-    console.log(sumOfExpense);
-    return sumOfExpense;
+    return total;
   }
+
+  function sumOfExpense() {
+    const total = expense.reduce(
+      (sum, transaction) => sum + parseFloat(transaction.amount),
+      0
+    );
+    return total;
+  }
+
+  const totalIncome = sumOfIncome();
+  const totalExpenses = sumOfExpense();
+  const balance = totalIncome - totalExpenses;
 
   return (
     <div className="w-full p-8 m-8">
       <h1>Dashboard</h1>
-      <div className="mt-4 p-4 grid grid-cols-4 gap-4">
-        <div className="border-1 p-6 flex justify-between">
+      <div className="mt-4 p-4 grid grid-cols-4 gap-4 ">
+        <div className="border-1 p-6 flex justify-between items-center ">
           Balance<br></br>
-          {totalBalance()}
+          {balance}
           <PiggyBank />
         </div>
-        <div className="border-1 p-6 flex justify-between">
-          Income <BanknoteArrowUp />
+        <div className="border-1 p-6 flex justify-between items-center">
+          Income<br></br>
+          {totalIncome} <BanknoteArrowUp />
         </div>
-        <div className="border-1 p-6  flex justify-between">
-          Expenses <BanknoteArrowDown />
+        <div className="border-1 p-6  flex justify-between items-center">
+          Expenses<br></br>
+          {totalExpenses} <BanknoteArrowDown />
         </div>
-        <div className="border-1 p-6  flex justify-between">
-          Transaction Count:<br></br> {transactionList.length} <WalletCards />
+        <div className="border-1 p-6  flex justify-between items-center">
+          Transaction Count<br></br> {transactionList.length} <WalletCards />
         </div>
       </div>
     </div>
