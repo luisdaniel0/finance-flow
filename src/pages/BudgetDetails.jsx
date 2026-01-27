b5cb4818fea81a1fb2a3e9f5435201aab93010a1
Luis Sanchez <luisanchezdh@gmail.com>
2026-01-26 20:13:42 -0500

    added delete button functionality, budgets state gets filtered and returns array without that specific budget, deleting it from state and added Link navigation back to /budgets page after delete

--- src/pages/BudgetDetails.jsx
+++ src/pages/BudgetDetails.jsx
@@ -1,4 +1,7 @@
 import { useParams } from "react-router-dom";
+import { Trash2 } from "lucide-react";
+import { SquarePen } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const BudgetDetail = ({ transactionList, budgets, setBudgets }) => {
   const { id } = useParams();
@@ -18,25 +21,53 @@ const BudgetDetail = ({ transactionList, budgets, setBudgets }) => {
 
   const progressPercentage = (budgetCalculation / budget.amount) * 100;
 
+  function deleteBudget(budgetId) {
+    console.log(budgetId);
+    console.log(budgets);
+    // const deleted = budgets.filter((budget) => {
+    //   return budget.id !== budgetId;
+    // });
+    // setBudgets({ ...budgets, deleted });
+    setBudgets(budgets.filter((budget) => budget.id !== budgetId));
+  }
+
   return (
     <>
       <div className="w-full p-8 m-8">
-        <div className="flex flex-col w-100 h-45  rounded-lg border p-5 justify-around">
-          <div className="flex justify-between">
-            <h1 className="text-lg font-bold">{budget.name}</h1>
-            <p className="text-lg font-bold text-regal-blue">
-              ${budget.amount}
-            </p>
+        <div className="grid grid-cols-2">
+          <div className="flex flex-col w-100 h-45  rounded-lg border p-5 justify-around">
+            <div className="flex justify-between">
+              <h1 className="text-lg font-bold">{budget.name}</h1>
+              <p className="text-lg font-bold text-regal-blue">
+                ${budget.amount}
+              </p>
+            </div>
+            <div className="flex justify-between">
+              <span>${budgetCalculation} spent</span>
+              <span>${budget.amount - budgetCalculation} remaining</span>
+            </div>
+            <div className=" h-2 w-full bg-gray-300 rounded-xl  ">
+              <div
+                style={{ maxWidth: `${progressPercentage}%` }}
+                className={`h-full bg-[#646cff]`}
+              ></div>
+            </div>
           </div>
-          <div className="flex justify-between">
-            <span>${budgetCalculation} spent</span>
-            <span>${budget.amount - budgetCalculation} remaining</span>
-          </div>
-          <div className=" h-2 w-full bg-gray-300 rounded-xl  ">
-            <div
-              style={{ maxWidth: `${progressPercentage}%` }}
-              className={`h-full bg-[#646cff]`}
-            ></div>
+          <div className="flex  items-center gap-5">
+            <button className="flex gap-1 cursor-pointer border-2 p-4 rounded-lg bg-regal-blue">
+              <SquarePen />
+              <span>Edit</span>
+            </button>
+
+            <Link to="/budgets">
+              <button
+                className="cursor-pointer flex gap-1 p-4 bg-red-600 rounded-lg "
+                onClick={() => deleteBudget(budget.id)}
+              >
+                <Trash2 />
+                <span>Delete</span>
+              </button>
+            </Link>
           </div>
         </div>
         <h1 className="text-lg font-bold mt-12">
@@ -52,7 +83,10 @@ const BudgetDetail = ({ transactionList, budgets, setBudgets }) => {
         </div>
         <div>
           {recentTransactions.map((transaction) => (
-            <div className="bg-gray-800 grid grid-cols-4 p-2">
+            <div
+              className="bg-gray-800 grid grid-cols-4 p-2"
+              key={transaction.id}
+            >
               <div className="">
                 <span>{transaction.description}</span>
               </div>
@@ -61,6 +95,11 @@ const BudgetDetail = ({ transactionList, budgets, setBudgets }) => {
               </div>
               <div>
                 <span>{transaction.date}</span>
+              </div>
+              <div>
+                <button className="cursor-pointer">
+                  <Trash2 />
+                </button>
               </div>
             </div>
           ))}
