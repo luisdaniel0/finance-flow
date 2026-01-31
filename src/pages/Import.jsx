7a64c26a739b3043315f22139bf5bd85988b56b8
Luis Sanchez <luisanchezdh@gmail.com>
2026-01-31 00:04:19 -0500

    cleaned up UI

--- src/pages/Import.jsx
+++ src/pages/Import.jsx
@@ -26,7 +26,7 @@ const Imports = () => {
       entryDate.getFullYear() === currentDateYear
     );
   });
-  console.log(parsedImport);
+  console.log("parsed:", parsedImport);
   return (
     <div className="w-full p-8 m-8">
       <label>
@@ -39,7 +39,7 @@ const Imports = () => {
         />
       </label>
       {previewData.length > 0 && (
-        <div className="grid grid-cols-3 text-center">
+        <div className="grid grid-cols-3 text-center mt-15">
           <div>Name</div>
           <div>Amount</div>
           <div>Date</div>
@@ -61,14 +61,47 @@ const Imports = () => {
           </div>
         ))}
       </div>
-      <div className="flex justify-center mt-10">
+      {previewData.length > 0 && (
+        <>
+          {/* <div className="grid grid-cols-3 text-center">
+            <div>Name</div>
+            <div>Amount</div>
+            <div>Date</div>
+          </div> */}
+          <div className="flex justify-center mt-10">
+            <button className="rounded-lg p-3 bg-regal-blue font-bold cursor-pointer mr-10">
+              Import
+            </button>
+            <button className="rounded-lg p-3 bg-regal-blue font-bold cursor-pointer">
+              Cancel
+            </button>
+          </div>
+        </>
+      )}
+
+      {/* <div>
+        {previewData.slice(0, 20).map((data, index) => (
+          <div className="grid grid-cols-3 text-center" key={index}>
+            <div>
+              <span>{data.Description}</span>
+            </div>
+            <div>
+              <span>{data.Amount}</span>
+            </div>
+            <div>
+              <span>{data["Posting Date"]}</span>
+            </div>
+          </div>
+        ))}
+      </div> */}
+      {/* <div className="flex justify-center mt-10">
         <button className="rounded-lg p-3 bg-regal-blue font-bold cursor-pointer mr-10">
           Import
         </button>
         <button className="rounded-lg p-3 bg-regal-blue font-bold cursor-pointer">
           Cancel
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
